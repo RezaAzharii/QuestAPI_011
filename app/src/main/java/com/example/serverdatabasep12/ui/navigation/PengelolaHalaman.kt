c3f4ae1e9d0662c75291e8852da546ccfc0ecc08
RezaAzharii <rezaazhari867@gmail.com>
2024-12-29 20:02:09 +0700

    memperbarui route onDetailClick pada HomeScreen

--- app/src/main/java/com/example/serverdatabasep12/ui/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/serverdatabasep12/ui/navigation/PengelolaHalaman.kt
@@ -3,11 +3,15 @@ package com.example.serverdatabasep12.ui.navigation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.serverdatabasep12.ui.view.DestinasiDetail
 import com.example.serverdatabasep12.ui.view.DestinasiEntry
 import com.example.serverdatabasep12.ui.view.DestinasiHome
+import com.example.serverdatabasep12.ui.view.DetailScreen
 import com.example.serverdatabasep12.ui.view.EntryMhsScreen
 import com.example.serverdatabasep12.ui.view.HomeScreen
 
@@ -23,7 +27,9 @@ fun PengelolaHalaman(
         composable(DestinasiHome.route){
             HomeScreen(
                 navigateToItemEntry = {navController.navigate(DestinasiEntry.route)},
-                onDetailClick = {}
+                onDetailClick = { nim ->
+                    navController.navigate("${DestinasiDetail.route}/$nim")
+                }
             )
         }
         composable(DestinasiEntry.route){
