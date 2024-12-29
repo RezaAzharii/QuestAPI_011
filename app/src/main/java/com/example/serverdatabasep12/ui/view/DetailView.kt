b2461eec047dbc7dff7e0244b4ed2b1a40ac08fc
RezaAzharii <rezaazhari867@gmail.com>
2024-12-29 19:54:43 +0700

    membuat ComponentDetailMhs untuk struktur data dan ItemDetailMhs dengan card dengan isian ComponentDetailMhs

--- app/src/main/java/com/example/serverdatabasep12/ui/view/DetailView.kt
+++ app/src/main/java/com/example/serverdatabasep12/ui/view/DetailView.kt
@@ -1,5 +1,20 @@
 package com.example.serverdatabasep12.ui.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.serverdatabasep12.model.Mahasiswa
 import com.example.serverdatabasep12.ui.navigation.DestinasiNavigasi
 
 object DestinasiDetail: DestinasiNavigasi{
@@ -9,4 +24,49 @@ object DestinasiDetail: DestinasiNavigasi{
     val routesWithArg = "$route/{$NIM}"
 }
 
+@Composable
+fun ItemDetailMhs(
+    modifier: Modifier = Modifier,
+    mahasiswa: Mahasiswa
+) {
+    Card(
+        modifier = modifier.padding(16.dp),
+        shape = MaterialTheme.shapes.medium,
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            ComponentDetailMhs(judul = "NIM", isinya = mahasiswa.nim)
+            ComponentDetailMhs(judul = "Nama", isinya = mahasiswa.nama)
+            ComponentDetailMhs(judul = "Alamat", isinya = mahasiswa.alamat)
+            ComponentDetailMhs(judul = "Jenis Kelamin", isinya = mahasiswa.jenisKelamin)
+            ComponentDetailMhs(judul = "Kelas", isinya = mahasiswa.kelas)
+            ComponentDetailMhs(judul = "Angkatan", isinya = mahasiswa.angkatan)
+        }
+    }
+}
+
+@Composable
+fun ComponentDetailMhs(
+    modifier: Modifier =Modifier,
+    judul: String,
+    isinya: String,
+){
+    Column(modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start)
+    {
+        Text(
+            text = "$judul : ",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray
+        )
+        Text(
+            text = isinya,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
 
