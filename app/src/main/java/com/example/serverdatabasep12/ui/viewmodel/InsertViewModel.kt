29f0fceffddc709f2582b265619f6cef36438c70
RezaAzharii <rezaazhari867@gmail.com>
2024-12-29 13:40:03 +0700

    Membuat InsertViewModel untuk mengelola state dan menambahkan fungsi insert data Mahasiswa menggunakan MahasiswaRepository

--- app/src/main/java/com/example/serverdatabasep12/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/serverdatabasep12/ui/viewmodel/InsertViewModel.kt
@@ -1,6 +1,34 @@
 package com.example.serverdatabasep12.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.serverdatabasep12.model.Mahasiswa
+import com.example.serverdatabasep12.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+
+class InsertViewModel (
+    private val mhs: MahasiswaRepository
+): ViewModel(){
+    var uiState by mutableStateOf(InsertUiState())
+        private set
+
+    fun updateInsertMhsState(insertUiEvent: InsertUiEvent){
+        uiState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+
+    suspend fun insertMhs(){
+        viewModelScope.launch {
+            try {
+                mhs.insertMahasiswa(uiState.insertUiEvent.toMhs())
+            }catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+}
 
 data class InsertUiState(
     val insertUiEvent: InsertUiEvent = InsertUiEvent()
