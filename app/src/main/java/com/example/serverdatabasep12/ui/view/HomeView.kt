48ce71ed7b38c6212514b54dbbf4c3b82ddbcb98
RezaAzharii <rezaazhari867@gmail.com>
2024-12-29 14:30:26 +0700

    Menambahkan fungsi loading dan error

--- app/src/main/java/com/example/serverdatabasep12/ui/view/HomeView.kt
+++ app/src/main/java/com/example/serverdatabasep12/ui/view/HomeView.kt
@@ -1,5 +1,6 @@
 package com.example.serverdatabasep12.ui.view
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -8,10 +9,12 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
@@ -21,7 +24,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.example.serverdatabasep12.R
 import com.example.serverdatabasep12.model.Mahasiswa
 import com.example.serverdatabasep12.ui.navigation.DestinasiNavigasi
 
@@ -29,6 +35,41 @@ object DestinasiHome: DestinasiNavigasi{
     override val route ="home"
     override val titleRes = "Home Mhs"
 }
+
+@Composable
+fun OnLoading(
+    modifier: Modifier = Modifier
+){
+    Image(
+        modifier =  modifier.size(200.dp),
+        painter = painterResource(R.drawable.ic_launcher_foreground),
+        contentDescription = stringResource(R.string.loading)
+    )
+}
+
+@Composable
+fun OnError(
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    Column (
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center
+    ){
+        Image(
+            painter = painterResource(id=R.drawable.ic_launcher_foreground),
+            contentDescription = ""
+        )
+        Text(
+            text = stringResource(R.string.loading_failed),
+            modifier = Modifier.padding(16.dp)
+        )
+        Button(onClick = retryAction) {
+            Text(stringResource(R.string.retry))
+        }
+    }
+}
+
 
 @Composable
 fun MhsLayout(
